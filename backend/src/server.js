0ea06976d95f4d2aadb34c6b3f2ebda1a6067a28
DrFerb0902 <trandangkhoa7147@gmail.com>
2026-04-05 17:32:12 +0700

    fix conflict và file schema,các package

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,15 +5,9 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-<<<<<<< HEAD
-// Các Route cơ bản
-const categoryRoutes = require('./routes/categoryRoutes');
-const foodRoutes = require('./routes/foodRoutes');
-const ingredientRoutes = require('./routes/ingredientRoutes');
-const uploadRoutes = require('./routes/uploadRoutes');
-=======
+
 // ======================== IMPORT ROUTES ========================
->>>>>>> develop
+
 const promotionRoutes = require('./routes/promotionRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 const invoiceRoutes = require('./routes/invoiceRoutes');
@@ -21,21 +15,18 @@ const authRoutes = require('./routes/authRoutes');
 const tableRoutes = require('./routes/tableRoutes');
 const reservationRoutes = require('./routes/reservationRoutes');
 const userRoutes = require('./routes/userRoutes');
+const categoryRoutes = require('./routes/categoryRoutes');
+const foodRoutes = require('./routes/foodRoutes');
+const ingredientRoutes = require('./routes/ingredientRoutes');
+const uploadRoutes = require('./routes/uploadRoutes');
 
 // ======================== MIDDLEWARE ========================
 app.use(cors());
 app.use(express.json()); // Để server hiểu được req.body dưới dạng JSON
 app.use(express.urlencoded({ extended: true }));
 
-<<<<<<< HEAD
-// Sử dụng Route
-app.use('/api/v1/categories', categoryRoutes);
-app.use('/api/v1/foods', foodRoutes);
-app.use('/api/v1/ingredients', ingredientRoutes);
-app.use('/api/v1/uploads', uploadRoutes);
-=======
+
 // ======================== API ROUTES ========================
->>>>>>> develop
 app.use('/api/v1/promotions', promotionRoutes);
 app.use('/api/v1/orders', orderRoutes);
 app.use('/api/v1/invoices', invoiceRoutes);
@@ -43,6 +34,10 @@ app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/tables', tableRoutes);
 app.use('/api/v1/reservations', reservationRoutes);
 app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/categories', categoryRoutes);
+app.use('/api/v1/foods', foodRoutes);
+app.use('/api/v1/ingredients', ingredientRoutes);
+app.use('/api/v1/uploads', uploadRoutes);
 
 // ======================== CATCH ERROR & LISTEN ========================
 // Test server đang chạy
